9570c9c99405ff4b7223666da2dc01ab4ba035e2
Emil <bmachaku@gmail.com>
2017-01-27 16:22:25 +0300

    send feedback to notifiable staff

--- mrejesho/views.py
+++ mrejesho/views.py
@@ -8,9 +8,13 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import Permission
+from django.contrib.auth import get_user_model
+from django.db.models import Q
 from .forms import FeedbackForm
 
 Feedback = apps.get_registered_model('mrejesho', 'Feedback')
+User = get_user_model()
 
 DEFAULT_FROM_EMAIL = getattr(settings, 'DEFAULT_FROM_EMAIL')
 EMAIL_FAIL_SILENTLY = getattr(settings, 'EMAIL_FAIL_SILENTLY', True)
@@ -26,14 +30,18 @@ class FeedbackCreate(CreateView):
 
     def form_valid(self, form):
         self.object = form.save()
+        site = get_current_site(self.request)
+        email_ctx = {
+            'site_name': site.name,
+            'site_domain': site.domain,
+            'feedback': self.object
+        }
+        self.notify_sender(site, email_ctx)
+        self.notify_staff(site, email_ctx)
+        return HttpResponseRedirect(self.get_success_url())
 
+    def notify_sender(self, email_ctx, site):
         if self.object.sender_email:
-            site = get_current_site(self.request)
-            email_ctx = {
-                'site_name': site.name,
-                'site_domain': site.domain,
-                'feedback': self.object
-            }
             delivered_message = render_to_string(
                 'mrejesho/emails/feedback_received.html', email_ctx)
             delivered_mail = EmailMessage(
@@ -43,7 +51,26 @@ class FeedbackCreate(CreateView):
                 [self.object.sender_email]
             )
             delivered_mail.send(fail_silently=EMAIL_FAIL_SILENTLY)
-        return HttpResponseRedirect(self.get_success_url())
+
+    def notify_staff(self, email_ctx, site):
+        perm = Permission.objects.get(
+            codename='can_receive_new_feedback_email')
+        notifiable_emails = User.objects.filter(
+            Q(groups__permissions=perm) | Q(user_permissions=perm)
+            ).distinct().values_list('email', flat=True)
+        notifiable_emails = list(filter(None, notifiable_emails))
+        if notifiable_emails:
+            notification_message = render_to_string(
+                'mrejesho/emails/new_feedback_received.html', email_ctx)
+            notification_mail = EmailMessage(
+                _('%(site_name)s: New Feedback received #%(feedback_id)'
+                %{'site_name': site.name, 'feedback_id': self.object.id}
+                ),
+                notification_message,
+                DEFAULT_FROM_EMAIL,
+                notifiable_email
+            )
+            notification_mail.send(fail_silently=EMAIL_FAIL_SILENTLY)
 
 
 class FeedbackThanks(TemplateView):
