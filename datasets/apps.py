6a8652beadafc0bd844a181f4d7fcdd20383ab1f
Emil <bmachaku@gmail.com>
2017-01-28 11:10:45 +0300

    update datasets app verbose name

--- datasets/apps.py
+++ datasets/apps.py
@@ -6,4 +6,4 @@ from django.utils.translation import ugettext_lazy as _
 
 class DatasetsConfig(AppConfig):
     name = 'datasets'
-    verbose_name = _('Datasets Suggestion')
+    verbose_name = _('Datasets Suggestions')
